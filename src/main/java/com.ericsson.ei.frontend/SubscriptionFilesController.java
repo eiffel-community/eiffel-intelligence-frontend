62caa805fc131414ca0e90fdd222d03408aff143
emichaf <emichaf@users.noreply.github.com>
2017-11-16 15:54:39 +0100

    new spring boot version, import cleanup (#4)

--- src/main/java/com.ericsson.ei.frontend/SubscriptionFilesController.java
+++ src/main/java/com.ericsson.ei.frontend/SubscriptionFilesController.java
@@ -16,44 +16,19 @@
 */  
 package com.ericsson.ei.frontend;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.omg.IOP.Encoding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.google.common.io.CharStreams;
-import com.sun.glass.ui.Application;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @Controller
 public class SubscriptionFilesController {
