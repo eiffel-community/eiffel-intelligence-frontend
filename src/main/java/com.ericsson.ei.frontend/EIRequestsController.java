92398dc325aaaf0e8754522267b20a6b2bcef6ca
Tobias Åkervik <tobias.akervik@ericsson.com>
2017-11-21 17:10:27 +0100

    Cleaned up code. Added logging framework and changed printouts to use logging instead.

--- src/main/java/com.ericsson.ei.frontend/EIRequestsController.java
+++ src/main/java/com.ericsson.ei.frontend/EIRequestsController.java
@@ -31,6 +31,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -39,7 +42,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HTTP;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -61,17 +63,16 @@ import org.springframework.web.servlet.ModelAndView;
 import org.apache.http.impl.client.HttpClients;
 
 
-//import reactor.core.publisher.Mono;
-
-
 @RestController
 @ConfigurationProperties(prefix="ei")
 //@RequestMapping(value = "")
 public class EIRequestsController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EIRequestsController.class);
+	
 	private String backendServerHost;
     private int backendServerPort;
-    
+
     private static final String APPLICATION_JSON = "application/json";
 
     // Backend host and port (Getter & Setters), application.properties -> greeting.xxx
@@ -102,12 +103,12 @@ public class EIRequestsController {
      */
     @CrossOrigin
     @RequestMapping(value = {"/subscriptions", "/subscriptions/*", "/information" }, method = RequestMethod.GET)
-    public ResponseEntity<?> getRequests(Model model, HttpServletRequest request) {
-    	System.out.println("Request with method GET.");
+    public ResponseEntity<String> getRequests(Model model, HttpServletRequest request) {
     	String eiBackendAddressSuffix = request.getServletPath();
-    	System.out.println("URL suffix received: " + eiBackendAddressSuffix);
     	String newRequestUrl = getEIBackendSubscriptionAddress() + eiBackendAddressSuffix;
-    	System.out.println("New EI request URL: " + newRequestUrl);
+    	LOG.info("Got HTTP Request with method GET.\nUrlSuffix: " + eiBackendAddressSuffix +
+    			"\nForwarding Request to EI Backend with url: " + newRequestUrl);
+
 
     	HttpClient client = HttpClients.createDefault();
     	HttpGet eiRequest = new HttpGet(newRequestUrl);
@@ -126,17 +127,21 @@ public class EIRequestsController {
     		inStream.close();
     	}
     	catch (IOException e) {
-    		System.out.println("ERROR CLIENT: " + e);
+    		LOG.error("Forward Request Errors: " + e);
     	}
 
-    	System.out.println("HTTP STATUS: " + eiResponse.getStatusLine().getStatusCode());
-    	System.out.println(JsonContent);
+    	LOG.debug("Http Status Code: " + eiResponse.getStatusLine().getStatusCode());
+    	LOG.debug("Recevied JsonContent:\n" + JsonContent);
     	
     	if (JsonContent.isEmpty()) {
     		JsonContent="[]";
     	}
+    	
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
     	ResponseEntity<String> responseEntity = new ResponseEntity<>(JsonContent,
+    			headers,
     			HttpStatus.valueOf(eiResponse.getStatusLine().getStatusCode()));
     	return responseEntity;
     }
@@ -149,13 +154,12 @@ public class EIRequestsController {
      */
 	@CrossOrigin
 	@RequestMapping(value = "/subscriptions", method = RequestMethod.POST)
-	public ResponseEntity<?> createRequests(Model model, HttpServletRequest request) {
+	public ResponseEntity<String> postRequests(Model model, HttpServletRequest request) {
 		
-    	System.out.println("Request with method POST.");
     	String eiBackendAddressSuffix = request.getServletPath();
-    	System.out.println("URL suffix received: " + eiBackendAddressSuffix);
     	String newRequestUrl = getEIBackendSubscriptionAddress() + eiBackendAddressSuffix;
-    	System.out.println("New EI request URL: " + newRequestUrl);
+    	LOG.info("Got HTTP Request with method POST.\nUrlSuffix: " + eiBackendAddressSuffix +
+    			"\nForwarding Request to EI Backend with url: " + newRequestUrl);
     	
     	String inputReqJsonContent = "";
     	try {
@@ -166,10 +170,10 @@ public class EIRequestsController {
     		inputBufReader.close();
     	}
     	catch (IOException e) {
-    		System.out.println("ERROR CLIENT: " + e);
+    		LOG.error("Forward Request Errors: " + e);
     	}
 
-    	System.out.println("Request JSON Content: " + inputReqJsonContent);
+    	LOG.debug("Input Request JSON Content to be forwarded:\n" + inputReqJsonContent);
     	HttpEntity inputReqJsonEntity = new ByteArrayEntity(inputReqJsonContent.getBytes());
     	
     	
@@ -193,17 +197,21 @@ public class EIRequestsController {
     		inStream.close();
     	}
     	catch (IOException e) {
-    		System.out.println("ERROR CLIENT: " + e);
+    		LOG.error("Forward Request Errors: " + e);
     	}
 
-    	System.out.println("HTTP STATUS: " + eiResponse.getStatusLine().getStatusCode());
-    	System.out.println(JsonContent);
-		
+    	LOG.debug("Http Status Code: " + eiResponse.getStatusLine().getStatusCode());
+    	LOG.debug("Recevied JsonContent: \n" + JsonContent);
+    	
     	if (JsonContent.isEmpty()) {
     		JsonContent="[]";
     	}
+
     	
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 		ResponseEntity<String> responseEntity = new ResponseEntity<>(JsonContent,
+				headers,
     			HttpStatus.valueOf(eiResponse.getStatusLine().getStatusCode()));
 		 return responseEntity;
 	}
@@ -214,13 +222,12 @@ public class EIRequestsController {
      */
 	@CrossOrigin
 	@RequestMapping(value = "/subscriptions", method = RequestMethod.PUT)
-	public ResponseEntity<?> putRequests(Model model, HttpServletRequest request) {
+	public ResponseEntity<String> putRequests(Model model, HttpServletRequest request) {
 		
-    	System.out.println("Request with method PUT.");
     	String eiBackendAddressSuffix = request.getServletPath();
-    	System.out.println("URL suffix received: " + eiBackendAddressSuffix);
     	String newRequestUrl = getEIBackendSubscriptionAddress() + eiBackendAddressSuffix;
-    	System.out.println("New EI request URL: " + newRequestUrl);
+    	LOG.info("Got HTTP Request with method PUT.\nUrlSuffix: " + eiBackendAddressSuffix +
+    			"\nForwarding Request to EI Backend with url: " + newRequestUrl);
     	
     	String inputReqJsonContent = "";
     	try {
@@ -231,10 +238,10 @@ public class EIRequestsController {
     		inputBufReader.close();
     	}
     	catch (IOException e) {
-    		System.out.println("ERROR CLIENT: " + e);
+    		LOG.error("Forward Request Errors: " + e);
     	}
 
-    	System.out.println("Request JSON Content: " + inputReqJsonContent);
+    	LOG.debug("Input Request JSON Content to be forwarded:\n" + inputReqJsonContent);
     	HttpEntity inputReqJsonEntity = new ByteArrayEntity(inputReqJsonContent.getBytes());
     	
     	
@@ -258,18 +265,23 @@ public class EIRequestsController {
     		inStream.close();
     	}
     	catch (IOException e) {
-    		System.out.println("ERROR CLIENT: " + e);
+    		LOG.error("Forward Request Errors: " + e);
     	}
 
-    	System.out.println("HTTP STATUS: " + eiResponse.getStatusLine().getStatusCode());
-    	System.out.println(JsonContent);
+    	LOG.debug("Http Status Code: " + eiResponse.getStatusLine().getStatusCode());
+    	LOG.debug("Recevied JsonContent:\n" + JsonContent);
 		
     	if (JsonContent.isEmpty()) {
     		JsonContent="[]";
     	}
-    	
-		ResponseEntity<String> responseEntity = new ResponseEntity<>(JsonContent,
-    			HttpStatus.valueOf(eiResponse.getStatusLine().getStatusCode()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>(JsonContent,
+				headers,
+				HttpStatus.valueOf(eiResponse.getStatusLine().getStatusCode()));
+
 		 return responseEntity;
 	}
 
@@ -280,12 +292,11 @@ public class EIRequestsController {
      */
     @CrossOrigin
     @RequestMapping(value = "/subscriptions/*", method = RequestMethod.DELETE)
-    public ResponseEntity<?> deleteRequests(Model model, HttpServletRequest request) {
-    	System.out.println("Request with method GET.");
+    public ResponseEntity<String> deleteRequests(Model model, HttpServletRequest request) {
     	String eiBackendAddressSuffix = request.getServletPath();
-    	System.out.println("URL suffix received: " + eiBackendAddressSuffix);
     	String newRequestUrl = getEIBackendSubscriptionAddress() + eiBackendAddressSuffix;
-    	System.out.println("New EI request URL: " + newRequestUrl);
+    	LOG.info("Got HTTP Request with method DELETE.\nUrlSuffix: " + eiBackendAddressSuffix +
+    			"\nForwarding Request to EI Backend with url: " + newRequestUrl);
 
     	HttpClient client = HttpClients.createDefault();
     	HttpDelete eiRequest = new HttpDelete(newRequestUrl);
@@ -304,17 +315,21 @@ public class EIRequestsController {
     		inStream.close();
     	}
     	catch (IOException e) {
-    		System.out.println("ERROR CLIENT: " + e);
+    		LOG.error("Forward Request Errors: " + e);
     	}
 
-    	System.out.println("HTTP STATUS: " + eiResponse.getStatusLine().getStatusCode());
-    	System.out.println(JsonContent);
+    	LOG.debug("Http Status Code: " + eiResponse.getStatusLine().getStatusCode());
+    	LOG.debug("Recevied JsonContent:\n" + JsonContent);
     	
     	if (JsonContent.isEmpty()) {
     		JsonContent="[]";
     	}
-
-    	ResponseEntity<String> responseEntity = new ResponseEntity<>(JsonContent,
+    	
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    	
+        ResponseEntity<String> responseEntity = new ResponseEntity<>(JsonContent,
+        		headers,
     			HttpStatus.valueOf(eiResponse.getStatusLine().getStatusCode()));
     	return responseEntity;
     }
