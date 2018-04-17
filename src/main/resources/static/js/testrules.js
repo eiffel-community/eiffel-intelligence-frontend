b5e61f1cc6b7243bc3918258d105e6d4ece956c1
RAJA MARAGANI <raja.maragani@ericsson.com>
2018-04-17 16:01:17 +0200

    modified add rules, and each event show in textarea like as rules

--- src/main/resources/static/js/testrules.js
+++ src/main/resources/static/js/testrules.js
@@ -1,6 +1,25 @@
 // Global vars
 var frontendServiceUrl;
 var i = 0;
+var ruleTemplate = {
+  "TemplateName" : "",
+  "Type" : "",
+  "TypeRule" : "",
+  "IdRule" : "",
+  "StartEvent" : "",
+  "IdentifyRules" : "",
+  "MatchIdRules" : {},
+  "ExtractionRules" : "",
+  "DownstreamIdentifyRules" : "",
+  "DownstreamMergeRules" : "",
+  "DownstreamExtractionRules" : "",
+  "ArrayMergeOptions" : "",
+  "HistoryIdentifyRules" : "",
+  "HistoryExtractionRules" : "",
+  "HistoryPathRules" : "",
+  "ProcessRules" : null,
+  "ProcessFunction" : null
+};
 jQuery(document).ready(
     function() {
 
@@ -51,41 +70,37 @@ jQuery(document).ready(
       function AppViewModel(rulesList) {
         var self = this;
         self.rulesBindingList = ko.observableArray(rulesList);
-        self.eventsBindingList = ko.observable();
-        self.dropdown = ko.observableArray([ "EiffelArtifactCreatedEvent", "EiffelArtifactPublishedEvent", "EiffelConfidenceLevelModifiedEvent",
-            "EiffelTestCaseStartedEvent", "EiffelTestCaseFinishedEvent", "EiffelCompositionDefinedEvent", "EiffelSourceChangeCreatedEvent",
-            "EiffelSourceChangeSubmittedEvent" ]);
-        self.backupdropdown = self.dropdown;
+        self.eventsBindingList = ko.observableArray([]);
         self.parsedToString = function(item) {
           return JSON.stringify(item, null, 2);
         };
-
-        //After adding a rule, this function remove the type from dropdown
-        self.removeDropdown = function(name) {
-          var index = self.dropdown.indexOf(name);
-          if (index !== -1) {
-            self.dropdown.splice(index, 1);
-          }
-          return name;
-        };
-
         // Removing the rule
-        self.removeRule = function(name) {
-          self.rulesBindingList.remove(name);
-          self.dropdown.push(name.Type);
-          if(self.rulesBindingList().length == 0){
+        self.removeRule = function(data, event) {
+          var context = ko.contextFor(event.target);
+          self.rulesBindingList.splice(context.$index(), 1);
+          if (self.rulesBindingList().length == 0) {
             $.jGrowl("Deleted all rule types, but we need atleast one Rule type, Here add default rule type", {
               sticky : false,
               theme : 'Error'
             });
-            self.rulesBindingList.push({
-              "TemplateName" : "",
-              "Type" : "EiffelArtifactCreatedEvent"
-
-            });
+            self.rulesBindingList.push(ruleTemplate);
           }
         };
-        
+
+        // Removing the events
+        self.removeEvent = function(data, event) {
+          var context = ko.contextFor(event.target);
+          self.eventsBindingList.splice(context.$index(), 1);
+          if (self.eventsBindingList().length == 0) {
+            self.eventsBindingList.push({});
+            $.jGrowl("Deleted all events, but we need atleast one event.", {
+              sticky : false,
+              theme : 'Error'
+            });
+            
+          }
+        };
+
         //This submit function for finding the aggregated object from the rules and events, This function internally call the ajax call
         self.submit = function() {
           var events = $("#eventsListID").val();
@@ -94,7 +109,20 @@ jQuery(document).ready(
             try {
               formRules.push(JSON.parse($(this).val()));
             } catch (e) {
-              $.jGrowl("Invalid json format :\n" + $(this).val(), {
+              $.jGrowl("Invalid json rule format :\n" + $(this).val(), {
+                sticky : false,
+                theme : 'Error'
+              });
+              return false;
+            }
+          });
+          
+          var formEvents = [];
+          $('.formEvents').each(function() {
+            try {
+              formEvents.push(JSON.parse($(this).val()));
+            } catch (e) {
+              $.jGrowl("Invalid json event format :\n" + $(this).val(), {
                 sticky : false,
                 theme : 'Error'
               });
@@ -131,116 +159,100 @@ jQuery(document).ready(
             complete : function() {
             }
           };
-          var eventsValid = isValidJSON(events.toString());
-          if (!eventsValid) {
-            alert("Events are not a valid json format");
-          } else {
+          
             var ajaxHttpSender = new AjaxHttpSender();
             //console.log(JSON.stringify(JSON.parse('{"listRulesJson":' + JSON.stringify(formRules) + ',"listEventsJson":' + events.toString() + '}')));
             ajaxHttpSender.sendAjax(frontendServiceUrl + "/rules/rule-check/aggregation", "POST", JSON.stringify(JSON.parse('{"listRulesJson":'
-                + JSON.stringify(formRules) + ',"listEventsJson":' + events.toString() + '}')), callback);
-          }
+                + JSON.stringify(formRules) + ',"listEventsJson":' + JSON.stringify(formEvents) + '}')), callback);
         };
 
         // This function for adding rule
-        self.addRule = function(viewModel, event) {
-          var newValue = event.target.value;
-          if (newValue != '') {
-            self.rulesBindingList.push({
-              "TemplateName" : "",
-              "Type" : newValue
-
-            });
-            self.removeDropdown(newValue);
-          }
-        }
+        self.addRule = function() {
+          self.rulesBindingList.push(ruleTemplate);
+        };
+        // This function for adding rule
+        self.addEvent = function() {
+          self.eventsBindingList.push({});
+        };
         return self;
       }
 
-      
       var vm = new AppViewModel([]);
       ko.applyBindings(vm, $("#submitButton")[0]);
-      vm.rulesBindingList.push({
-        "TemplateName" : "",
-        "Type" : "EiffelArtifactCreatedEvent"
+      vm.rulesBindingList.push(ruleTemplate);
+      vm.eventsBindingList.push({});
 
-      });
-      vm.removeDropdown("EiffelArtifactCreatedEvent");
-      ko.applyBindings(vm, $("#testTulesDOMObject")[0]);
+      ko.applyBindings(vm, $("#testRulesDOMObject")[0]);
+      ko.applyBindings(vm, $("#testEventsDOMObject")[0]);
 
       //Upload events list json data
-      $(".container").on("click","button.upload_rules", function(event) {
-            event.stopPropagation();
-            event.preventDefault();
+      $(".container").on("click", "button.upload_rules", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
 
-            function validateJsonAndCreateSubscriptions(subscriptionFile) {
-              var reader = new FileReader();
-              reader.onload = function() {
-                var fileContent = reader.result;
-                var jsonLintResult = "";
-                try {
-                  jsonLintResult = jsonlint.parse(fileContent);
-                } catch (e) {
-                  $.alert("JSON Format Check Failed:\n" + e.name + "\n" + e.message);
-                  return false;
-                }
-                $.jGrowl('JSON Format Check Succeeded', {
-                  sticky : false,
-                  theme : 'Notify'
-                });
-
-                var rulesList = JSON.parse(fileContent);
-                ko.cleanNode($("#testTulesDOMObject")[0]);
-                ko.cleanNode($("#submitButton")[0]);
-                $("#testTulesDOMObject").css('min-height', $(".navbar-sidenav").height() - 180);
-                vm.rulesBindingList.removeAll();
-                $('.eventsListDisplay > div:gt(0)').remove();
-                vm.rulesBindingList = ko.observableArray(rulesList);
-                vm.dropdown([]);
-                vm.dropdown = ko.observableArray([ "EiffelArtifactCreatedEvent", "EiffelArtifactPublishedEvent",
-                    "EiffelConfidenceLevelModifiedEvent", "EiffelTestCaseStartedEvent", "EiffelTestCaseFinishedEvent",
-                    "EiffelCompositionDefinedEvent", "EiffelSourceChangeCreatedEvent", "EiffelSourceChangeSubmittedEvent" ]);
-
-                ko.applyBindings(vm, $("#testTulesDOMObject")[0]);
-                ko.applyBindings(vm, $("#submitButton")[0]);
-              };
-              reader.readAsText(subscriptionFile);
+        function validateJsonAndCreateSubscriptions(subscriptionFile) {
+          var reader = new FileReader();
+          reader.onload = function() {
+            var fileContent = reader.result;
+            var jsonLintResult = "";
+            try {
+              jsonLintResult = jsonlint.parse(fileContent);
+            } catch (e) {
+              $.alert("JSON Format Check Failed:\n" + e.name + "\n" + e.message);
+              return false;
             }
+            $.jGrowl('JSON Format Check Succeeded', {
+              sticky : false,
+              theme : 'Notify'
+            });
 
-            function createUploadWindow() {
-              var pom = document.createElement('input');
-              pom.setAttribute('id', 'uploadFile');
-              pom.setAttribute('type', 'file');
-              pom.setAttribute('name', 'upFile');
-              pom.onchange = function uploadFinished() {
-                var subscriptionFile = pom.files[0];
-                validateJsonAndCreateSubscriptions(subscriptionFile);
-              };
-              if (document.createEvent) {
-                var event = document.createEvent('MouseEvents');
-                event.initEvent('click', true, true);
-                pom.dispatchEvent(event);
-              } else {
-                pom.click();
-              }
-            }
+            var rulesList = JSON.parse(fileContent);
+            ko.cleanNode($("#testRulesDOMObject")[0]);
+            ko.cleanNode($("#submitButton")[0]);
+            $("#testRulesDOMObject").css('min-height', $(".navbar-sidenav").height() - 180);
+            vm.rulesBindingList.removeAll();
+            $('.rulesListDisplay > div:gt(0)').remove();
+            vm.rulesBindingList = ko.observableArray(rulesList);
+            ko.applyBindings(vm, $("#testRulesDOMObject")[0]);
+            ko.applyBindings(vm, $("#submitButton")[0]);
+          };
+          reader.readAsText(subscriptionFile);
+        }
 
-            function createUploadWindowMSExplorer() {
-              $('#upload_rules').click();
-              var file = $('#upload_rules').prop('files')[0];
-              validateJsonAndCreateSubscriptions(file);
-            }
+        function createUploadWindow() {
+          var pom = document.createElement('input');
+          pom.setAttribute('id', 'uploadFile');
+          pom.setAttribute('type', 'file');
+          pom.setAttribute('name', 'upFile');
+          pom.onchange = function uploadFinished() {
+            var subscriptionFile = pom.files[0];
+            validateJsonAndCreateSubscriptions(subscriptionFile);
+          };
+          if (document.createEvent) {
+            var event = document.createEvent('MouseEvents');
+            event.initEvent('click', true, true);
+            pom.dispatchEvent(event);
+          } else {
+            pom.click();
+          }
+        }
 
-            // If MS Internet Explorer -> special handling for creating
-            // download
-            // file window.
-            if (window.navigator.msSaveOrOpenBlob) {
-              createUploadWindowMSExplorer();
-            } else {
-              // HTML5 Download File window handling
-              createUploadWindow();
-            }
-          });
+        function createUploadWindowMSExplorer() {
+          $('#upload_rules').click();
+          var file = $('#upload_rules').prop('files')[0];
+          validateJsonAndCreateSubscriptions(file);
+        }
+
+        // If MS Internet Explorer -> special handling for creating
+        // download
+        // file window.
+        if (window.navigator.msSaveOrOpenBlob) {
+          createUploadWindowMSExplorer();
+        } else {
+          // HTML5 Download File window handling
+          createUploadWindow();
+        }
+      });
 
       //Upload list of events json data
       $(".container").on("click", "button.upload_events", function(event) {
@@ -262,12 +274,13 @@ jQuery(document).ready(
               sticky : false,
               theme : 'Notify'
             });
-
+            console.log("I am ghere");
             var eventsList = JSON.parse(fileContent);
-            ko.cleanNode($("#submitButton")[0]);
-            $("#eventsListID").val(JSON.stringify(eventsList, null, 2));
-            $(".textareaCustom").css('min-height', $(".navbar-sidenav").height() - 180);
-            ko.applyBindings(vm, $("#submitButton")[0]);
+            ko.cleanNode($("#testEventsDOMObject")[0]);
+            vm.eventsBindingList.removeAll();
+            $('.eventsListDisplay > div:gt(0)').remove();
+            vm.eventsBindingList = ko.observableArray(eventsList);
+            ko.applyBindings(vm, $("#testEventsDOMObject")[0]);
           };
           reader.readAsText(subscriptionFile);
         }
@@ -345,8 +358,7 @@ jQuery(document).ready(
           } else {
             downloadFile(jsonData, contentType, fileName);
           }
-        }
-        else{
+        } else {
           $.jGrowl("Data not available for download!", {
             sticky : false,
             theme : 'Error'
