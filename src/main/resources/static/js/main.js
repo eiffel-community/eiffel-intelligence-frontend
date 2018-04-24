d1b50b17d1395889a26e567d9642723332eda7c3
eztyhva <valentin.tyhonov@ericsson.com>
2018-04-24 10:01:46 +0300

    Fixed review comments

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,80 +1,79 @@
 
 jQuery(document).ready(function() {
-	
-    // Fetch injected URL from DOM 
-    var eiffelDocumentationUrlLinks = $('#eiffelDocumentationUrlLinks').text();
+
+	// Fetch injected URL from DOM
+	var eiffelDocumentationUrlLinks = $('#eiffelDocumentationUrlLinks').text();
 	var frontendServiceUrl = $('#frontendServiceUrl').text();
 
-    function loadMainPage() {
-    	$("#mainFrame").load("subscriptionpage.html");
+	function loadMainPage() {
+		$("#mainFrame").load("subscriptionpage.html");
 	}
 
 	$("#testRulesBtn").click(function() {
-    	$("#mainFrame").load("testRules.html");
+		$("#mainFrame").load("testRules.html");
 	});
-	
+
 	$("#eiInfoBtn").click(function() {
-    	$("#mainFrame").load("eiInfo.html");
+		$("#mainFrame").load("eiInfo.html");
 	});
-	
+
 	$("#loginBtn").click(function() {
 		$("#mainFrame").load("login.html");
 	});
-	
+
 	$("#logoutBtn").click(function() {
 		$.ajax({
-		    url : "/auth/logout",
-		    type : "GET",
-		    contentType : 'application/json; charset=utf-8',
-		    cache: false,
-		    complete : function (XMLHttpRequest, textStatus) {
-		        sessionStorage.removeItem("currentUser");
-		        $("#userName").text("Guest");
-		        $("#loginBlock").show();
-		        $("#logoutBlock").hide();
-		        loadMainPage();
-		    }
+			url : "/auth/logout",
+			type : "GET",
+			contentType : 'application/json; charset=utf-8',
+			cache: false,
+			complete : function (XMLHttpRequest, textStatus) {
+				sessionStorage.removeItem("currentUser");
+				$("#userName").text("Guest");
+				$("#loginBlock").show();
+				$("#logoutBlock").hide();
+				loadMainPage();
+			}
 		});
 	});
-	
+
 	$("#subscriptionBtn").click(function() {
-    	loadMainPage();
+		loadMainPage();
 	});
 
 	$("#jmesPathRulesSetUpBtn").click(function() {
-    	$("#mainFrame").load("jmesPathRulesSetUp.html");
-    });
+		$("#mainFrame").load("jmesPathRulesSetUp.html");
+	});
 
 	function loadDocumentLinks(){
 		// eiffelDocumentationUrlLinks variable is configure in application.properties
 		var linksList = JSON.parse(eiffelDocumentationUrlLinks);
-
-	    var docLinksDoc = document.getElementById('collapseDocPages');
+		var docLinksDoc = document.getElementById('collapseDocPages');
 		var liTag = null;
 		var aTag = null;
-		
-    	Object.keys(linksList).forEach(function(linkKey) {
-    			liTag = document.createElement('li');
-    			aTag = document.createElement('a');
-    			aTag.innerHTML = linkKey;
-    			aTag.setAttribute('href', linksList[linkKey]);
-    			aTag.setAttribute('target', '_blanc');
-    			liTag.appendChild(aTag);
-    			docLinksDoc.appendChild(liTag);
-    	    	});
+
+		Object.keys(linksList).forEach(function(linkKey) {
+			liTag = document.createElement('li');
+			aTag = document.createElement('a');
+			aTag.innerHTML = linkKey;
+			aTag.setAttribute('href', linksList[linkKey]);
+			aTag.setAttribute('target', '_blanc');
+			liTag.appendChild(aTag);
+			docLinksDoc.appendChild(liTag);
+		});
 	}
-	
+
 	var initOneTime = function(){
 		initOneTime = function(){}; // kill it as soon as it was called
 		loadDocumentLinks();
-	    loadMainPage();
+		loadMainPage();
 	};
 
-    var currentUser = sessionStorage.getItem("currentUser");
+	var currentUser = sessionStorage.getItem("currentUser");
 	if(currentUser != "") {
-	    $("#userName").text(currentUser);
-	    $("#logoutBlock").show();
+		$("#userName").text(currentUser);
+		$("#logoutBlock").show();
 	}
-	
+
 	initOneTime();
 });
