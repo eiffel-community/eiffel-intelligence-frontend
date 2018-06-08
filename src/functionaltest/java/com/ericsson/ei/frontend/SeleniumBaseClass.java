ab6558746637f535f892241730f31a0dc98c1448
Erik <erik.edling@ericsson.com>
2018-06-08 10:43:41 +0200

    Remove unnecessary code

--- src/functionaltest/java/com/ericsson/ei/frontend/SeleniumBaseClass.java
+++ src/functionaltest/java/com/ericsson/ei/frontend/SeleniumBaseClass.java
@@ -32,7 +32,6 @@ public class SeleniumBaseClass {
     protected WebDriver driver;
     protected String baseUrl;
 
-    private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
 
     @Before
@@ -47,39 +46,6 @@ public class SeleniumBaseClass {
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
         }
     }
 }
