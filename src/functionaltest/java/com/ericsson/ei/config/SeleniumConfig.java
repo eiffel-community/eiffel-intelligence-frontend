0ef04ddcffcd1198a21543a2eaf611b789504753
eroohen <henning.roos@ericsson.com>
2018-06-08 09:08:25 +0200

    Fix logging

--- src/functionaltest/java/com/ericsson/ei/config/SeleniumConfig.java
+++ src/functionaltest/java/com/ericsson/ei/config/SeleniumConfig.java
@@ -5,17 +5,24 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ericsson.ei.frontend.exception.OSNotSupportedException;
 
 public class SeleniumConfig {
 
-    public static FirefoxDriver getFirefoxDriver() {
+    private static final Logger log = LoggerFactory.getLogger(SeleniumConfig.class);
+
+    public static FirefoxDriver getFirefoxDriver() throws OSNotSupportedException {
         FirefoxDriver driver;
         if(SystemUtils.IS_OS_LINUX) {
             System.setProperty("webdriver.gecko.driver", "src/functionaltest/resources/geckodriver");
         } else if (SystemUtils.IS_OS_WINDOWS) {
             System.setProperty("webdriver.gecko.driver", "src/functionaltest/resources/geckodriver.exe");
         } else {
-            System.exit(1);
+            log.error("OS currently not supported.");
+            throw new OSNotSupportedException();
         }
         FirefoxOptions firefoxOptions = new FirefoxOptions().setHeadless(true);
 
@@ -25,7 +32,7 @@ public class SeleniumConfig {
     }
 
     public static String getBaseUrl(int randomServerPort) {
-        String baseUrl = "http://localhost:" + Integer.toString(randomServerPort);
+        String baseUrl = "http://localhost:" + randomServerPort;
         return baseUrl;
     }
 }
