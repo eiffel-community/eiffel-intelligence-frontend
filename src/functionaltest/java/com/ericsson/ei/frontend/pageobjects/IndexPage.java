a825f983818e4b448181fad2b152a0acefd91c9b
Mohammad Saifullah <mohammad.saifullah@ericsson.com>
2018-06-28 09:44:09 +0200

    index page

--- src/functionaltest/java/com/ericsson/ei/frontend/pageobjects/IndexPage.java
+++ src/functionaltest/java/com/ericsson/ei/frontend/pageobjects/IndexPage.java
@@ -4,20 +4,16 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
-
 import org.mockito.Mockito;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 import java.io.IOException;
 
 public class IndexPage extends PageBaseClass {
-
     public IndexPage(CloseableHttpClient mockedHttpClient, FirefoxDriver driver,
-
             String baseUrl) throws ClientProtocolException, IOException {
         super(mockedHttpClient, driver, baseUrl);
     }
@@ -33,13 +29,12 @@ public class IndexPage extends PageBaseClass {
     }
 
     public TestRulesPage clickTestRulesPage() throws ClientProtocolException, IOException {
-    	WebElement testRulesBtn = driver.findElement(By.id("testRulesBtn"));
+        WebElement testRulesBtn = driver.findElement(By.id("testRulesBtn"));
         testRulesBtn.click();
 
         TestRulesPage testRulesPage = new TestRulesPage(mockedHttpClient, driver, baseUrl);
         return testRulesPage;
     }
-
 
     public void clickReloadButton(String responseData) throws ClientProtocolException, IOException {
         CloseableHttpResponse response = this.createMockedHTTPResponse(responseData, 200);
@@ -47,11 +42,9 @@ public class IndexPage extends PageBaseClass {
         Mockito.doReturn(response).when(mockedHttpClient).execute(Mockito.argThat(request ->
             ((HttpRequestBase)request).getURI().toString().contains("subscriptions")));
 
-
         WebElement reloadButton = driver.findElement(By.className("table_reload"));
         reloadButton.click();
     }
-
 
     public Object presenceOfReloadButton() {
         try {
@@ -61,5 +54,4 @@ public class IndexPage extends PageBaseClass {
             return false;
         }
     }
-
 }
