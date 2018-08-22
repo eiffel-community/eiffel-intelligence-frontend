9deab1680b17c07193a125f1fc5709d5395aad73
eznedan <andriy.nedvyha@ericsson.com>
2018-08-22 21:03:29 +0300

    fixes

--- src/main/resources/static/js/errorMessages.js
+++ src/main/resources/static/js/errorMessages.js
@@ -1,6 +1,6 @@
 var errorsStore = new Array();
-if(sessionStorage.getItem('errorsStore')){
-    let messageErr = JSON.parse(sessionStorage.getItem('errorsStore'));
+let messageErr = JSON.parse(sessionStorage.getItem('errorsStore'));
+if(messageErr){
     for(var i=0; i<messageErr.length; i++){
         errorsStore.push(messageErr[i]);
     }
@@ -9,13 +9,12 @@ function viewModel(data){
     this.errorMessages = ko.observableArray(data);
 }
 function getErrors(){
-    let messageErr = JSON.parse(sessionStorage.getItem('errorsStore'));
-    let observableObject = $("#qwerty")[0];
+    let observableObject = $("#alerts")[0];
     ko.cleanNode(observableObject);
-    ko.applyBindings(new viewModel(messageErr), observableObject);
+    ko.applyBindings(new viewModel(errorsStore), observableObject);
 }
 function logMessages(messageErr){
     $.jGrowl(messageErr, {sticky: false, theme: 'Error'});
     errorsStore.push({message:messageErr});
-    localStorage.setItem('errorsStore', JSON.stringify(errorsStore));
+    sessionStorage.setItem('errorsStore', JSON.stringify(errorsStore));
 }
