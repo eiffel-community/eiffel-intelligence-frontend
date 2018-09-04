ed06e8f9e3381ed9e2ca7faced1d6aa605fd48c5
eznedan <andriy.nedvyha@ericsson.com>
2018-09-04 14:17:29 +0300

    FIX: entries generation issue

--- src/main/resources/static/js/errorMessages.js
+++ src/main/resources/static/js/errorMessages.js
@@ -1,20 +1,46 @@
 var errorsStore = new Array();
-let messageErr = JSON.parse(sessionStorage.getItem('errorsStore'));
-if(messageErr){
-    for(var i=0; i<messageErr.length; i++){
-        errorsStore.push(messageErr[i]);
+let msg = JSON.parse(sessionStorage.getItem('errorsStore'));
+if(msg){
+    for(var i=0; i<msg.length; i++){
+        errorsStore.push(msg[i]);
     }
 }
-function viewModel(data){
-    this.errorMessages = ko.observableArray(data);
+function viewModel(message){
+    this.message = ko.observable(message);
 }
-function getErrors(){
-    let observableObject = $("#alerts")[0];
-    ko.cleanNode(observableObject);
-    ko.applyBindings(new viewModel(errorsStore), observableObject);
+function model(data){
+    var self = this;
+    self.errorMessages = ko.observableArray([]);
+    var json = JSON.parse(ko.toJSON(data));
+    for(var i = 0; i < json.length; i++) {
+        var obj = json[i];
+    	let msgErr = new viewModel(obj.message);
+    	self.errorMessages.push(msgErr);
+    }
 }
 function logMessages(messageErr){
     $.jGrowl(messageErr, {sticky: false, theme: 'Error'});
     errorsStore.push({message:messageErr});
     sessionStorage.setItem('errorsStore', JSON.stringify(errorsStore));
+    $('div.dropdown-menu').replaceWith("<div id=\"alerts\" style=\"display: none;\" class=\"dropdown-menu\" aria-labelledby=\"alertsDropdown\" data-bind=\"foreach: errorMessages\">" +
+                                            "<div class=\"dropdown-divider\"> </div>" +
+                                                "<a class=\"dropdown-item\">" +
+                                                    "<div class=\"dropdown-message small\" data-bind=\"text: message\"></div>" +
+                                                "</a>" +
+                                        "</div>");
+    ko.cleanNode($("#alerts")[0]);
+    ko.applyBindings(new model(errorsStore),$("#alerts")[0]);
 }
+function getErrors(){
+    var x = document.getElementById("alerts");
+    let msg = JSON.parse(sessionStorage.getItem('errorsStore'));
+    if (x.style.display === "none" && msg) {
+        x.style.display = "block";
+        return;
+    } else {
+        x.style.display = "none";
+        return;
+    }
+}
+ko.cleanNode($("#alerts")[0]);
+ko.applyBindings(new model(errorsStore),$("#alerts")[0]);
