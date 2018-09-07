beb782f4d829d06bebe75e37dad648988a582b3d
eznedan <andriy.nedvyha@ericsson.com>
2018-09-07 13:53:04 +0300

    fixes

--- src/functionaltest/java/com/ericsson/ei/frontend/TestAlarm.java
+++ src/functionaltest/java/com/ericsson/ei/frontend/TestAlarm.java
@@ -2,8 +2,10 @@ package com.ericsson.ei.frontend;
 
 import com.ericsson.ei.frontend.pageobjects.IndexPage;
 import com.ericsson.ei.frontend.pageobjects.TestRulesPage;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.io.IOException;
 import java.util.stream.IntStream;
@@ -14,10 +16,13 @@ public class TestAlarm extends SeleniumBaseClass {
 
     private static final String alarmResult = "Deleted all events, but we need atleast one event.\nDeleted all rule types, but we need atleast one Rule type, Here add default rule type";
 
+    @MockBean
+    protected CloseableHttpClient mockedHttpClient;
+
     @Test
     public void testAlarm() throws IOException {
         //Load index page
-        IndexPage indexPageObject = new IndexPage(null, driver, baseUrl);
+        IndexPage indexPageObject = new IndexPage(mockedHttpClient, driver, baseUrl);
         indexPageObject.loadPage();
 
         //Generate exception
