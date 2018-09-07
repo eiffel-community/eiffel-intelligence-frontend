3bd1c0a40d8f086a3bb217490530f6fc798f306f
eznedan <andriy.nedvyha@ericsson.com>
2018-09-07 14:48:06 +0300

    change test

--- src/functionaltest/java/com/ericsson/ei/frontend/TestAlarm.java
+++ src/functionaltest/java/com/ericsson/ei/frontend/TestAlarm.java
@@ -2,10 +2,8 @@ package com.ericsson.ei.frontend;
 
 import com.ericsson.ei.frontend.pageobjects.IndexPage;
 import com.ericsson.ei.frontend.pageobjects.TestRulesPage;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.io.IOException;
 import java.util.stream.IntStream;
@@ -14,15 +12,10 @@ import static org.junit.Assert.assertEquals;
 
 public class TestAlarm extends SeleniumBaseClass {
 
-    private static final String alarmResult = "Deleted all events, but we need atleast one event.\nDeleted all rule types, but we need atleast one Rule type, Here add default rule type";
-
-    @MockBean
-    protected CloseableHttpClient mockedHttpClient;
-
     @Test
     public void testAlarm() throws IOException {
         //Load index page
-        IndexPage indexPageObject = new IndexPage(mockedHttpClient, driver, baseUrl);
+        IndexPage indexPageObject = new IndexPage(null, driver, baseUrl);
         indexPageObject.loadPage();
 
         //Generate exception
@@ -32,6 +25,6 @@ public class TestAlarm extends SeleniumBaseClass {
 
         //Click alarm button few times
         IntStream.range(0, 5).forEachOrdered(i -> indexPageObject.clickAlarmButton());
-        assertEquals(alarmResult, driver.findElement(By.id("alerts")).getText());
+        assertEquals(3, driver.findElements(By.className("dropdown-item")).size());
     }
 }
--- src/functionaltest/java/com/ericsson/ei/frontend/TestRulesFunctionality.java
+++ src/functionaltest/java/com/ericsson/ei/frontend/TestRulesFunctionality.java
@@ -5,12 +5,14 @@ import com.ericsson.ei.frontend.pageobjects.IndexPage;
 import com.ericsson.ei.frontend.pageobjects.TestRulesPage;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,6 +32,7 @@ public class TestRulesFunctionality extends SeleniumBaseClass {
             File.separator, "src", "functionaltest", "resources", "responses", "EventsTemplateObject.json");
     private static final String AGGREGATED_OBJECT_FILE_PATH = String.join(
             File.separator, "src", "functionaltest", "resources", "responses", "AggregatedObjectResponse.json");
+    private static final String alarmResult = "Deleted all events, but we need atleast one event.\nDeleted all rule types, but we need atleast one Rule type, Here add default rule type";
 
     @Test
     public void testJourneyToFindAggregatedObjectButton() throws Exception {
@@ -89,5 +92,9 @@ public class TestRulesFunctionality extends SeleniumBaseClass {
         String findAggregatedObjectResponse = getJSONStringFromFile(AGGREGATED_OBJECT_FILE_PATH);
         testRulesPage.clickFindAggregatedObject(findAggregatedObjectResponse);
         assertEquals(findAggregatedObjectResponse, testRulesPage.getAggregatedResultData());
+
+        //Click alarm button few times
+        IntStream.range(0, 5).forEachOrdered(i -> indexPageObject.clickAlarmButton());
+        assertEquals(alarmResult, driver.findElement(By.id("alerts")).getText());
     }
 }
