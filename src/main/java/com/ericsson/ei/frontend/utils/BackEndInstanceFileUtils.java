7092c470ca5faee837c48e8e6ca96811db2f0437
Tobias Akervik <tobias.akervik@ericsson.com>
2018-11-23 15:17:54 +0100

    Removed unused imports

--- src/main/java/com/ericsson/ei/frontend/utils/BackEndInstanceFileUtils.java
+++ src/main/java/com/ericsson/ei/frontend/utils/BackEndInstanceFileUtils.java
@@ -7,8 +7,7 @@ import java.nio.file.Paths;
 
 import javax.annotation.PostConstruct;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
