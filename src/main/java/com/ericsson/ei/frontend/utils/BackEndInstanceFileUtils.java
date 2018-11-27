1671ee97983ee1d2d86f250e59798a63d4a4ced0
Tobias Akervik <tobias.akervik@ericsson.com>
2018-11-27 09:49:03 +0100

    Fixed some indentations.

--- src/main/java/com/ericsson/ei/frontend/utils/BackEndInstanceFileUtils.java
+++ src/main/java/com/ericsson/ei/frontend/utils/BackEndInstanceFileUtils.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 
 import javax.annotation.PostConstruct;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,10 +35,10 @@ public class BackEndInstanceFileUtils {
 
     @Autowired
     BackEndInstancesUtils backendInstancesUtils;
-    
+
     @Value("${ei.backendInstancesFilePath:#{null}}")
     private String backendInstancesFilePath;
-    
+
     @Value("${ei.backendInstancesListJsonContent:#{null}}")
     private String backendInstancesListJsonContent;
 
@@ -47,9 +46,8 @@ public class BackEndInstanceFileUtils {
     public void init() throws IOException {
         LOG.info("Initiating BackEndInstanceFileUtils.");
 
-
         // Use home folder if a specific backendInstancesFilePath isn't provided
-        if(backendInstancesFilePath == null || backendInstancesFilePath.isEmpty()) {
+        if (backendInstancesFilePath == null || backendInstancesFilePath.isEmpty()) {
 
             String homeFolder = System.getProperty("user.home");
             String eiHome = Paths.get(homeFolder, EI_HOME_DEFAULT_NAME).toString();
@@ -60,30 +58,28 @@ public class BackEndInstanceFileUtils {
             }
 
             setEiInstancesPath(Paths.get(eiHome, BACKEND_INSTANCES_DEFAULT_FILENAME).toString());
-            LOG.info("EI Instances List file path is not provided! " +
-                     "Will create a default EI Instances List file at file path: " + eiInstancesPath);
-            
+            LOG.info("EI Instances List file path is not provided! "
+                    + "Will create a default EI Instances List file at file path: " + eiInstancesPath);
+
             parseAndSetEiInstancesList();
-            
+
         } else {
             setEiInstancesPath(Paths.get(backendInstancesFilePath).toString());
             if (!(new File(eiInstancesPath).isFile())) {
-            	LOG.info("EI Instances List file don' exist! Creating file with given or " +
-                         "default EI instances list content. File path: " + eiInstancesPath);
+                LOG.info("EI Instances List file don' exist! Creating file with given or "
+                        + "default EI instances list content. File path: " + eiInstancesPath);
                 createFileWithDirs();
                 parseAndSetEiInstancesList();
+            } else {
+                LOG.info("EI-Backend instances list file that will be used: " + eiInstancesPath);
             }
-            else {
-            	LOG.info("EI-Backend instances list file that will be used: " + eiInstancesPath);
-            }
-            
+
         }
     }
 
-
     /**
-     * Parses Ei Instances list from backendInstancesListJsonContent property
-     * and sets default EI-Backend instance.
+     * Parses Ei Instances list from backendInstancesListJsonContent property and
+     * sets default EI-Backend instance.
      * 
      */
     private void parseAndSetEiInstancesList() {
@@ -91,45 +87,40 @@ public class BackEndInstanceFileUtils {
         parsedBackendInstancesListJsonArray = parseEiInstancesListJsonObject();
         dumpJsonArray(parsedBackendInstancesListJsonArray);
         try {
-			ensureValidFile();
-		} catch (IOException e) {
-			     LOG.error("Failed to validate EI Instances List json object." +
-		               "\nError message: " + e.getMessage() +
-		               "\nErrors: " + e);
-		}
+            ensureValidFile();
+        } catch (IOException e) {
+            LOG.error("Failed to validate EI Instances List json object." + "\nError message: " + e.getMessage()
+                    + "\nErrors: " + e);
+        }
         setDefaultEiBackendInstance(parsedBackendInstancesListJsonArray);
     }
-
 
     /**
      * Parses Ei Instances list from backendInstancesListJsonContent property.
      * 
-     * @return
-     *      JsonArray
+     * @return JsonArray
      */
     private JsonArray parseEiInstancesListJsonObject() {
         JsonArray backendInstancesListJsonArray = null;
-        
-        if ( backendInstancesListJsonContent == null || backendInstancesListJsonContent.isEmpty()) {
-        	LOG.error("EI backned instances list json object is empty, can't continue." +
-                      "\nMake sure that EI Instances list flags is set, " +
-        			  " 'ei.backendInstancesFilePath' or 'ei.backendInstancesListJsonContent'");
-      	System.exit(1);
+
+        if (backendInstancesListJsonContent == null || backendInstancesListJsonContent.isEmpty()) {
+            LOG.error("EI backned instances list json object is empty, can't continue."
+                    + "\nMake sure that EI Instances list flags is set, "
+                    + " 'ei.backendInstancesFilePath' or 'ei.backendInstancesListJsonContent'");
+            System.exit(1);
         }
-        
+
         try {
-        	backendInstancesListJsonArray = (JsonArray) new JsonParser().parse(backendInstancesListJsonContent.toString());
-        }
-        catch (JsonSyntaxException e) {
-        	LOG.error("Failed to parse EI backned instances list json object." +
-                      "\nMake sure ei.backendInstancesListJsonContent properties is set with one or more EI Backend instances." +
-        			  "\nError message: " + e.getMessage() +
-        			  "\nErrors: " + e);
-        	System.exit(1);
+            backendInstancesListJsonArray = (JsonArray) new JsonParser()
+                    .parse(backendInstancesListJsonContent.toString());
+        } catch (JsonSyntaxException e) {
+            LOG.error("Failed to parse EI backned instances list json object."
+                    + "\nMake sure ei.backendInstancesListJsonContent properties is set with one or more EI Backend instances."
+                    + "\nError message: " + e.getMessage() + "\nErrors: " + e);
+            System.exit(1);
         }
         return backendInstancesListJsonArray;
     }
-
 
     /**
      * Sets default EI Backend instance.
@@ -140,25 +131,25 @@ public class BackEndInstanceFileUtils {
             JsonObject jObject = instanceJsonObj.getAsJsonObject();
             if (Boolean.getBoolean(jObject.get("defaultBackend").toString())) {
                 backendInstancesUtils.setDefaultBackEndInstance(jObject.get(BackEndInstancesUtils.NAME).toString(),
-                jObject.get(BackEndInstancesUtils.HOST).toString(),
-                Integer.parseInt(jObject.get(BackEndInstancesUtils.PORT).toString()),
-                jObject.get(BackEndInstancesUtils.CONTEXT_PATH).toString(),
-                Boolean.getBoolean(jObject.get(BackEndInstancesUtils.DEFAULT).toString()));
+                        jObject.get(BackEndInstancesUtils.HOST).toString(),
+                        Integer.parseInt(jObject.get(BackEndInstancesUtils.PORT).toString()),
+                        jObject.get(BackEndInstancesUtils.CONTEXT_PATH).toString(),
+                        Boolean.getBoolean(jObject.get(BackEndInstancesUtils.DEFAULT).toString()));
             }
         }
     }
 
     /**
-     *Gets the JSON-data from file and returns as a JsonArray
+     * Gets the JSON-data from file and returns as a JsonArray
      *
-     * @return
-     *      JsonArray
+     * @return JsonArray
      */
     public JsonArray getInstancesFromFile() {
         try {
             ensureValidFile();
 
-            JsonArray inputBackEndInstances = new JsonParser().parse(new String(Files.readAllBytes(Paths.get(eiInstancesPath)))).getAsJsonArray();
+            JsonArray inputBackEndInstances = new JsonParser()
+                    .parse(new String(Files.readAllBytes(Paths.get(eiInstancesPath)))).getAsJsonArray();
             return inputBackEndInstances;
         } catch (IOException e) {
             LOG.error("Failure when try to parse json file" + e.getMessage());
@@ -169,8 +160,7 @@ public class BackEndInstanceFileUtils {
     /**
      * Saves the given JsonArray
      *
-     * @param jsonArrayToDump
-     *      JsonArray
+     * @param jsonArrayToDump JsonArray
      */
     public void dumpJsonArray(JsonArray jsonArrayToDump) {
         try {
@@ -190,12 +180,14 @@ public class BackEndInstanceFileUtils {
             }
 
             if (!fileContainsJsonArray()) {
-                LOG.error("File does not contain valid json! JSON:'" + new String(Files.readAllBytes(Paths.get(eiInstancesPath))) + "'.");
+                LOG.error("File does not contain valid json! JSON:'"
+                        + new String(Files.readAllBytes(Paths.get(eiInstancesPath))) + "'.");
                 System.exit(-1);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             String message = String.format(
-                    "Failed to read backendInstancesFilePath %s. Please check access rights or choose another backendInstancesFilePath in application.properties.", eiInstancesPath);
+                    "Failed to read backendInstancesFilePath %s. Please check access rights or choose another backendInstancesFilePath in application.properties.",
+                    eiInstancesPath);
             LOG.error(message);
             System.exit(-1);
         }
@@ -204,8 +196,9 @@ public class BackEndInstanceFileUtils {
     private void createFileWithDirs() throws IOException {
         File eiInstancesParentFolder = Paths.get(eiInstancesPath).getParent().toFile();
 
-        if (!(eiInstancesParentFolder.isDirectory())){
-            LOG.info(String.format("Parentdir(s) for %s does not exist! Trying to create necessary parent dirs.", backendInstancesFilePath));
+        if (!(eiInstancesParentFolder.isDirectory())) {
+            LOG.info(String.format("Parentdir(s) for %s does not exist! Trying to create necessary parent dirs.",
+                    backendInstancesFilePath));
             eiInstancesParentFolder.mkdirs();
         }
 
@@ -229,7 +222,8 @@ public class BackEndInstanceFileUtils {
 
         if (!success) {
             String message = String.format(
-                    "Failed to create eiffel intelligence home folder in %s. Please check access rights or choose a specific backendInstancesFilePath in application.properties.", eiHome);
+                    "Failed to create eiffel intelligence home folder in %s. Please check access rights or choose a specific backendInstancesFilePath in application.properties.",
+                    eiHome);
             LOG.error(message);
             System.exit(-1);
         }
