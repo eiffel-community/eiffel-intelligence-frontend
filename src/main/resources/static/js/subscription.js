2208edca33f5c53da3c9ed0d284cef07fcd7eadd
Anders Breid <anders.breid@ericsson.com>
2018-11-28 13:33:07 +0100

    Removed Chrille

--- src/main/resources/static/js/subscription.js
+++ src/main/resources/static/js/subscription.js
@@ -109,10 +109,6 @@ jQuery(document).ready(function () {
         }
     }, 1000);
 
-    $(".control").click(function() {
-        console.log("Hej Chrille!");
-    });
-
     // Check if EI Backend Server is online when Status Connection button is pressed.
     $("#btnEIConnection").click(function () {
         checkBackendStatus();
