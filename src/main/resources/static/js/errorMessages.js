4d15d2aa2269449ec6f26c18d2303eaab9e4dbcd
Christoffer Cortes Sjöwall <christoffer.cortes.sjowall@ericsson.com>
2018-12-06 14:38:32 +0100

    Fix margins and restructure how errormessages are handled

--- src/main/resources/static/js/errorMessages.js
+++ src/main/resources/static/js/errorMessages.js
@@ -1,48 +1,42 @@
-var errorsStore = new Array();
-let msg = JSON.parse(sessionStorage.getItem('errorsStore'));
-if(msg){
-    for(var i=0; i<msg.length; i++){
-        errorsStore.push(msg[i]);
-    }
-}
-function viewModel(message){
+function messageModel(message){
     this.message = ko.observable(message);
 }
-function model(data){
+function viewModel(data){
     var self = this;
     self.errorMessages = ko.observableArray([]);
-    var json = JSON.parse(ko.toJSON(data));
-    json.reverse();
-    for(var i = 0; i < json.length; i++) {
-        var obj = json[i];
-    	let msgErr = new viewModel(obj.message);
-    	self.errorMessages.push(msgErr);
+    var errorsStore = new Array();
+
+    self.init = function() {
+        var storage = sessionStorage.getItem('errorsStore');
+        if(storage != undefined && storage != "") {
+            let msg = JSON.parse(storage);
+            for(var i=0; i<msg.length; i++){
+                errorsStore.push(msg[i]);
+            }
+        }
+        var json = JSON.parse(ko.toJSON(errorsStore));
+        for(var i = 0; i < json.length; i++) {
+            var obj = json[i];
+            self.addErrorMessage(obj.message);
+        }
+    }
+    self.addErrorMessage = function (data) {
+        console.log(data);
+        let msgErr = new messageModel(data);
+        self.errorMessages.push(msgErr);
+    };
+    self.storeErrorMessage = function (data) {
+        errorsStore.push({message:data});
+        sessionStorage.setItem('errorsStore', JSON.stringify(errorsStore));
     }
 }
+var vm = new viewModel();
+vm.init();
+ko.cleanNode($("#alerts")[0]);
+ko.applyBindings(vm,$("#alerts")[0]);
+
 function logMessages(messageErr){
     $.jGrowl(messageErr, {sticky: false, theme: 'Error', position:'center'});
-    errorsStore.push({message:messageErr});
-    sessionStorage.setItem('errorsStore', JSON.stringify(errorsStore));
-    $('div.dropdown-menu').replaceWith("<div id=\"alerts\" style=\"display: none;\" class=\"dropdown-menu\" aria-labelledby=\"alertsDropdown\" data-bind=\"foreach: errorMessages\">" +
-                                            "<div class=\"dropdown-divider\"> </div>" +
-                                                "<a class=\"dropdown-item\">" +
-                                                    "<div class=\"dropdown-message small\" data-bind=\"text: message, attr: {title: message}\"></div>" +
-                                                "</a>" +
-                                        "</div>");
-    ko.cleanNode($("#alerts")[0]);
-    ko.applyBindings(new model(errorsStore),$("#alerts")[0]);
+    vm.addErrorMessage(messageErr);
+    vm.storeErrorMessage(messageErr);
 }
-$(document).mouseup(function(e){
-    var container = $("#alerts");
-    var bell = $("i.fa.fa-fw.fa-bell");
-    var click = $("#alertsDropdown");
-    var x = document.getElementById("alerts");
-    let msg = JSON.parse(sessionStorage.getItem('errorsStore'));
-    if (!container.is(e.target) && container.has(e.target).length === 0 && x.style.display === "block"){
-        container.hide();
-    } else if(msg && x.style.display === "none" && (click.is(e.target) || bell.is(e.target))){
-        container.show();
-    }
-});
-ko.cleanNode($("#alerts")[0]);
-ko.applyBindings(new model(errorsStore),$("#alerts")[0]);
