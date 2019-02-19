53e5f71e05e2924c1e00a06374229a99f077c433
Erik Edling <38752030+e-edling-ericsson@users.noreply.github.com>
2019-02-19 09:47:52 +0100

    Add subscriptions for artifact jobs (#174)
    * Add subscriptions in feature files

--- src/systemtest/java/com/ericsson/ei/systemtest/utils/Config.java
+++ src/systemtest/java/com/ericsson/ei/systemtest/utils/Config.java
@@ -12,6 +12,9 @@ public class Config {
     private String jenkinsPassword;
 
     private String remremBaseUrl;
+
+    private String eiFrontendBaseUrl;
+    private String eiBackendBaseUrl;
     /**
     *
     * This method retrieves the jenkins properties from the system properties.
@@ -19,9 +22,9 @@ public class Config {
     * @return
     */
     public void initJenkinsConfig() {
-        jenkinsBaseUrl = System.getProperty("JENKINS_BASE_URL");
-        jenkinsUsername = System.getProperty("JENKINS_USERNAME");
-        jenkinsPassword = System.getProperty("JENKINS_PASSWORD");
+        jenkinsBaseUrl = System.getProperty("jenkins.base.url");
+        jenkinsUsername = System.getProperty("jenkins.username");
+        jenkinsPassword = System.getProperty("jenkins.password");
 
         assertNotNull(jenkinsBaseUrl);
         assertNotNull(jenkinsUsername);
@@ -35,8 +38,26 @@ public class Config {
     * @return
     */
     public void initRemRemConfig() {
-        remremBaseUrl = System.getProperty("REMREM_BASE_URL");
+        remremBaseUrl = System.getProperty("remrem.base.url");
 
         assertNotNull(remremBaseUrl);
+    }
+
+    /**
+     *
+     * This method retrieves the EI front end properties from the system properties.
+     *
+     * @return
+     */
+    public void initEIFrontend() {
+        eiFrontendBaseUrl = System.getProperty("ei.frontend.base.url");
+
+        assertNotNull(eiFrontendBaseUrl);
+    }
+
+    public void initEIBackend() {
+        eiBackendBaseUrl = System.getProperty("ei.backend.base.url");
+
+        assertNotNull(eiBackendBaseUrl);
     }
 }
