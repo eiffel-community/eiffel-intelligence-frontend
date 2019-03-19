cc4d10b54a55a7d9ee5853337525eeea59352970
Emelie Pettersson <emelie.pettersson@ericsson.com>
2019-03-19 16:42:05 +0100

    Add REMREM json list in env.bash

--- src/main/docker/env.bash
+++ src/main/docker/env.bash
@@ -13,3 +13,7 @@ export EIFFEL2_EI_FRONTEND_EI_INSTANCES_LIST="[\
 { \"contextPath\": \"\", \"port\": \"8080\", \"name\": \"ei-backend-testexecution\", \"host\": \"ei-backend-testexecution\", \"https\": false, \"defaultBackend\": false}\
 ]"
 
+
+export REMREM_PUBLISH_RABBITMQ_INSTANCES_LIST="[\
+{ \"mp\": \"eiffelsemantics\", \"host\": \"rabbitmq\", \"port\": \"5672\", \"username\": \"myuser\", \"password\": \"myuser\", \"tls\": \"\", \"exchangeName\": \"ei-exchange\", \"domainId\": \"ei-domain\" }\
+]"
