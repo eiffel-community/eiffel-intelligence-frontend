b3b6ee86e3e878fa9788669021b893d7027b16a8
Christoffer Cortes Sjöwall <35095827+Christoffer-Cortes@users.noreply.github.com>
2019-07-17 14:16:20 +0200

    Add page that displays rules from GET /rules endpoint (#228)
    * Add page for the rules under the Eiffel Intelligence Info menu item.

--- src/main/java/com/ericsson/ei/frontend/EIRequestsController.java
+++ src/main/java/com/ericsson/ei/frontend/EIRequestsController.java
@@ -71,7 +71,7 @@ public class EIRequestsController {
      */
     @CrossOrigin
     @RequestMapping(value = { "/subscriptions", "/subscriptions/*", "/information", "/download/*", "/auth", "/auth/*",
-            "/queryAggregatedObject", "/queryMissedNotifications", "/query",
+            "/queryAggregatedObject", "/queryMissedNotifications", "/query", "/rules",
             "/rules/rule-check/testRulePageEnabled" }, method = RequestMethod.GET)
     public ResponseEntity<String> getRequests(Model model, HttpServletRequest incomingRequest) {
         String eiRequestUrl;
--- wiki/markdown/GUI-overview.md
+++ wiki/markdown/GUI-overview.md
@@ -3,8 +3,9 @@
 Eiffel Intelligence Frontend GUI can be divided into two parts a navigation
 window and a work window.
 
-<img src="images/overview.png">
-</img>
+<kbd>
+    <img style="border:1px solid black" src="images/overview.png"></img>
+</kbd>
 
 As the name suggests, the navigation bar provides its users one click access to
 the resources to interact with EI. This window contains some buttons. Clicking
