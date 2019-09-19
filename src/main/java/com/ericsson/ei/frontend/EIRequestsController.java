38586cb10a61de39de2bb22599ccc029fa2129f6
Anders Breid <38723221+a-breid-ericsson@users.noreply.github.com>
2019-09-19 12:41:24 +0200

    Add support to use /status endoint (#259)
    - Back-end status check will now be performed on the /status endpoint
      instead of /auth/checkStatus.
    - Updated documentation.

--- src/main/java/com/ericsson/ei/frontend/EIRequestsController.java
+++ src/main/java/com/ericsson/ei/frontend/EIRequestsController.java
@@ -71,7 +71,7 @@ public class EIRequestsController {
      */
     @CrossOrigin
     @RequestMapping(value = { "/subscriptions", "/subscriptions/*", "/information", "/download/*", "/auth", "/auth/*",
-            "/queryAggregatedObject", "/queryMissedNotifications", "/query", "/rules",
+            "/queryAggregatedObject", "/queryMissedNotifications", "/query", "/rules", "/status",
             "/rules/rule-check/testRulePageEnabled" }, method = RequestMethod.GET)
     public ResponseEntity<String> getRequests(Model model, HttpServletRequest incomingRequest) {
         String eiRequestUrl;
--- src/main/resources/static/js/global-variables.js
+++ src/main/resources/static/js/global-variables.js
@@ -12,7 +12,7 @@ var usernameMaxDisplayLength = 15;
 var backendEndpoints = {
     AUTH: "/auth",
     LOGIN: "/auth/login",
-    CHECK_STATUS: "/auth/checkStatus",
+    CHECK_STATUS: "/status",
     SUBSCRIPTIONS: "/subscriptions",
     DOWNLOAD: "/download/",
     DOWNLOAD_SUBSCRIPTIONS_TEMPLATE: "/download/subscriptionsTemplate",
@@ -22,7 +22,6 @@ var backendEndpoints = {
     CHECK_AGGREGATION: "/rules/rule-check/aggregation"
 };
 
-// Status handling variables
 var PAGES_FOR_STATUS_INDICATION = "subscriptions test-rules ei-info login";
 var statusType = {
     SUCCESS: "alert-success",
@@ -31,7 +30,7 @@ var statusType = {
     DANGER: "alert-danger"
 };
 var statusText = {
-    BACKEND_DOWN: "<strong>Back-end is down!</strong> Wait for it go up or switch to another back-end before continuing!",
+    BACKEND_DOWN: "<strong>Back-end, MongoDB and/or RabbitMQ is down!</strong> Wait for them go up or switch to another back-end before continuing!",
     UNKNOWN_BACK_END_STATUS: "<strong>Back-end status is unknown!</strong> Wait for it to update or switch to another back-end before continuing!",
     TEST_RULES_DISABLED: "<strong>Test Rule service is disabled!</strong> To enable it set the backend property [testaggregated.enabled] as [true]"
 };
@@ -40,9 +39,6 @@ var backEndStatus = true;
 var backEndStatusUpdated = false;
 var previousBackEndStatus;
 var backEndStatusTimerInterval;
-// End Status variables
-
-// Start ## getters and setters
 
 function isLdapEnabled(){
     return Boolean(ldapEnabled);
@@ -69,9 +65,21 @@ function getFrontendServiceBackEndPath() {
     return frontendServiceBackEndPath;
 }
 
-// End   ## getters and setters
+function getSubscriptionNameRegex() {
+    return subscriptionNameRegex;
+}
 
-// Start ## getters and setters status handling
+function getNotificationMetaRegex() {
+    return notificationMetaRegex;
+}
+
+function getUsernameMaxDisplayLength() {
+    if (usernameMaxDisplayLength < 4) {
+        // Minimum leangth is 4 due to ellipsis being 3 long
+        return 4;
+    }
+    return usernameMaxDisplayLength;
+}
 
 function isBackEndStatusOk() {
     return Boolean(backEndStatus);
@@ -105,21 +113,3 @@ function setBackEndStatusTimerInterval() {
 function getWhiteListedPages() {
     return String(PAGES_FOR_STATUS_INDICATION);
 }
-
-function getSubscriptionNameRegex() {
-    return subscriptionNameRegex;
-}
-
-function getNotificationMetaRegex() {
-    return notificationMetaRegex;
-}
-
-function getUsernameMaxDisplayLength() {
-    if (usernameMaxDisplayLength < 4) {
-        // Minimum leangth is 4 due to ellipsis being 3 long
-        return 4;
-    }
-    return usernameMaxDisplayLength;
-}
-
-// End   ## getters and setters status handling
